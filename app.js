a237393933300d0948f3ed2a44ad1e732161c47e
killajk2201 <killajk2201@gmail.com>
2019-01-19 23:34:52 -0600

    First Commit

---
+++ app.js
@@ -0,0 +1,96 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+var expressValidator = require('express-validator');
+var LocalStrategy = require('passport-local').Strategy;
+var multer = require('multer');
+var upload = multer({dest: './public/images/profileimages'})
+var flash = require('connect-flash');
+var mongodb = require('mongodb');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+var bcrypt = require('bcryptjs');
+var schedule = require('node-schedule');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// Handle File Uploads
+//app.use(multer({dest: './uploads'}));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle Sessions
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value){
+      var namespace = param.split('.'),
+          root = namespace.shift(),
+          formParam = root;
+
+      while(namespace.length){
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
+    }
+}));
+app.use(flash());
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req,res);
+  next();
+});
+
+app.get('*', function(req, res, next){
+    res.locals.user = req.user || null;
+    next();
+})
+
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ models/user.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nodeauth');
+
+var db = mongoose.connection;
+
+var userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        index: true
+    },
+    password: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    profileImage: {
+        type: String
+    }
+});
+
+var User = module.exports = mongoose.model('User', userSchema);
+
+module.exports.createUser = function(newUser, callback){
+    bcrypt.genSalt(10, function(err, salt){
+        bcrypt.hash(newUser.password, salt, function(err, hash){
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+};
+module.exports.getUserById = function(id, callback){
+    User.findById(id, callback);
+};
+module.exports.getUserByUsername = function(username, callback){
+    var query = {username: username};
+    User.findOne(query, callback);
+};
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+    bcrypt.compare(candidatePassword, hash, function(err, isMatch){
+        callback(null, isMatch);
+    });
+};
